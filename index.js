355ab237c245116c968430c333d895d1fd4ba1d8
ConnorBradshaw <38018502+ConnorBradshaw@users.noreply.github.com>
2018-11-17 21:04:03 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -28,92 +28,59 @@ function bats(batterID, batter, pitcherID, pitcher, date, launchAngle, velocity,
 	this.outcome = outcome;
 	this.video = video;
 }
-
-var ball = new Array();
-var i = 0;
-d3.csv("BattedBallData.csv", function(d){
-	for(var count = 0; count < d.length; count++)
-	{
-	ball.push(new bats());
-	ball[count].batterID = d[count].BATTER_ID;
-	ball[count].batter = d[count].BATTER,
-	ball[count].pitcherID = d[count].PITCHER_ID,
-	ball[count].pitcher = d[count].PITCHER,
-	ball[count].date = d[count].GAME_DATE,
-	ball[count].launchAngle = d[count].LAUNCH_ANGLE,
-	ball[count].velocity = d[count].EXIT_SPEED,
-	ball[count].angle = d[count].EXIT_DIRECTION,
-	ball[count].distance = d[count].HIT_DISTANCE,
-	ball[count].hangTime = d[count].HANG_TIME,
-	ball[count].spinRate = d[count].HIT_SPIN_RATE,
-	ball[count].outcome = d[count].PLAY_OUTCOME,
-	ball[count].video = d[count].VIDEO_LINK
-}
-ball.sort(function(x, y){
-   return d3.ascending(x.batter, y.batter);
-})
-for(var p = 0; p < ball.length; p++)
+function myFunction(name, j, k, video, angle, distance, outcome, velocity)
 {
-
-	function myFunction(j, k, video, angle, distance, outcome, velocity)
+	if(k.toLowerCase().includes(name) || name == null)
 	{
-		if(outcome == "Undefined")
+		if(outcome == "Undefined"){}
+		else
 		{
-		}
-		else{
 
-		use = "h" + j;
-		used = ".h" + j;
-		uses = ".h" + j + " svg";
-		if(!$( used ).length)
-		{
-			var div = document.createElement('div');
-			div.classList.add(use);
-			div.classList.add("float");
-			div.classList.add("sample");
-			document.body.appendChild(div);
-			
-			var sampleSVG = d3.select(used)
-			.append("svg:svg")
-			.attr("class", "sample")
-			.attr("width", 600)
-			.attr("height", 468);
-			d3.select(used)
-			.append("text")
-			.text(k)
-			.attr("class", "title");
+			use = "h" + j;
+			used = ".h" + j;
+			uses = ".h" + j + " svg";
+			if(!$( used ).length)
+			{
+				var div = document.createElement('div');
+				div.classList.add(use);
+				div.classList.add("float");
+				document.body.appendChild(div);
+				
+				var sampleSVG = d3.select(used)
+				.append("svg:svg")
+				.attr("class", "sample")
+				.attr("width", 600)
+				.attr("height", 468);
+				d3.select(used)
+				.append("text")
+				.text(k)
+				.attr("class", "title");
+				d3.select(uses)
+				.append("svg:image")
+				.attr('width', 600)
+				.attr('height', 468)
+				.attr("xlink:href", "baseball.png");
+				
+			}
+			theta = (angle * 1 + 90) * (Math.PI / 180);
 			d3.select(uses)
-			.append("svg:image")
-			.attr('width', 600)
-			.attr('height', 468)
-			.attr("xlink:href", "baseball.png");
-			
+			.append("svg:line")
+			.attr("id", ".test_div" + i)
+			.attr("stroke", "black")
+			.attr("x1", 300)
+			.attr("y1", 340)
+			.attr("x2", 300 + (distance * .65) * -Math.cos(theta))
+			.attr("y2", 340 + (distance * .65) * -Math.sin(theta))
+			.attr("stroke-width", 2.5)
+			.attr("stroke-opacity", .7)
+			.on("mouseover", function(){return tooltip.style("visibility", "visible"), tooltip.html("Outcome: " + outcome + "<br/> Distance: " + distance + " ft<br/> Velocity: " + velocity + " mph"), d3.select(this).attr("stroke-width", 4), d3.select(this).attr("stroke","white");  })
+			.on("mousemove", function(event){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
+			.on("mouseout", function(){return tooltip.style("visibility", "hidden"), d3.select(this).attr("stroke-width", 2.5), d3.select(this).attr("stroke","black");})
+			.on("mousedown", function(){window.open(video, 'newwindow');});
 		}
-		theta = (angle * 1 + 90) * (Math.PI / 180);
-		d3.select(uses)
-		.append("svg:line")
-		.attr("id", ".test_div" + i)
-		.attr("stroke", "black")
-		.attr("x1", 300)
-		.attr("y1", 340)
-		.attr("x2", 300 + (distance * .65) * -Math.cos(theta))
-		.attr("y2", 340 + (distance * .65) * -Math.sin(theta))
-		.attr("stroke-width", 2.5)
-		.attr("stroke-opacity", .7)
-		.on("mouseover", function(){return tooltip.style("visibility", "visible"), tooltip.html("Outcome: " + outcome + "<br/> Distance: " + distance + " ft<br/> Velocity: " + velocity + " mph"), d3.select(this).attr("stroke-width", 4), d3.select(this).attr("stroke","white");  })
-		.on("mousemove", function(event){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
-		.on("mouseout", function(){return tooltip.style("visibility", "hidden"), d3.select(this).attr("stroke-width", 2.5), d3.select(this).attr("stroke","black");})
-		.on("mousedown", function(){window.open(video, 'newwindow');});
 	}
+	else{}
 }
-
-	myFunction(ball[p].batterID, ball[p].batter, ball[p].video, ball[p].angle, ball[p].distance, ball[p].outcome, ball[p].velocity);
-	i++;
-
-
-
-}	
-});
 var tooltip = d3.select("body")
 	.append("div")
 	.attr("class", "border")
@@ -124,5 +91,44 @@ var tooltip = d3.select("body")
 	.style("z-index", "10")
 	.style("visibility", "hidden")
 	.text("a simple tooltip");
+var ball = new Array();
+var i = 0;
+d3.csv("BattedBallData.csv", function(d)
+{
+	for(var count = 0; count < d.length; count++)
+	{
+		ball.push(new bats());
+		ball[count].batterID = d[count].BATTER_ID;
+		ball[count].batter = d[count].BATTER,
+		ball[count].pitcherID = d[count].PITCHER_ID,
+		ball[count].pitcher = d[count].PITCHER,
+		ball[count].date = d[count].GAME_DATE,
+		ball[count].launchAngle = d[count].LAUNCH_ANGLE,
+		ball[count].velocity = d[count].EXIT_SPEED,
+		ball[count].angle = d[count].EXIT_DIRECTION,
+		ball[count].distance = d[count].HIT_DISTANCE,
+		ball[count].hangTime = d[count].HANG_TIME,
+		ball[count].spinRate = d[count].HIT_SPIN_RATE,
+		ball[count].outcome = d[count].PLAY_OUTCOME,
+		ball[count].video = d[count].VIDEO_LINK
+		var temp = ball[count].batter.split(",");
+		ball[count].batter = temp[1] + " " + temp[0];
+	}
+	ball.sort(function(x, y)
+	{
+	   return d3.ascending(x.batter, y.batter);
+	})
+	function createChart(name)
+	{
+		for(var p = 0; p < ball.length; p++)
+		{
+			myFunction(name, ball[p].batterID, ball[p].batter, ball[p].video, ball[p].angle, ball[p].distance, ball[p].outcome, ball[p].velocity);
+			i++;
+		}
+	}
+	createChart();
+
+});
+
 
 
